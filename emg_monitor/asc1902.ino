901162d08b623db7d33593c2e4f57ac90f1512fa
Wafi <ahmedw1@tcd.ie>
2025-02-19 13:42:23 +0000

    added EMG monitor code

--- emg_monitor/asc1902.ino
+++ emg_monitor/asc1902.ino
@@ -19,7 +19,28 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
  OLED_COPI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
 #define ANALOG_INPUT A0    // SINE WAVE INPUT FROM OP-AMP
-#define TRIGGER_LEVEL 512  // TO CENTER THE SINE WAVE
+// #define TRIGGER_LEVEL 512  // TO CENTER THE SINE WAVE
+int TRIGGER_LEVEL;
+
+int max_analog_dta = 300;   // max analog data
+int min_analog_dta = 100;   // min analog data
+int static_analog_dta = 0;  // static analog data
+
+
+int getAnalog(int pin) {
+  long sum = 0;
+  for (int i = 0; i < 32; i++) {
+    sum += analogRead(pin);
+  }
+
+  int dta = sum >> 5;
+
+  max_analog_dta = dta > max_analog_dta ? dta : max_analog_dta;  // if max data
+  min_analog_dta = min_analog_dta > dta ? dta : min_analog_dta;  // if min data
+
+  return sum >> 5;
+}
+
 
 void setup() {
   Serial.begin(9600);
@@ -32,9 +53,21 @@ void setup() {
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 10);
-  display.println("GROUP 12 OSCILLOSCOPE");
+  display.println("GROUP 12 EMG Monitor");
   display.display();
-  delay(1000);
+
+    long sum = 0;
+  for (int i = 0; i <= 10; i++) {
+    for (int j = 0; j < 100; j++) {
+      sum += getAnalog(A0);
+      delay(1);
+    }
+  }
+  sum /= 1100;
+  static_analog_dta = sum;
+  Serial.print("static_analog_dta = ");
+  Serial.println(static_analog_dta);
+  TRIGGER_LEVEL = static_analog_dta;
 }
 
 void loop() {
@@ -43,7 +76,8 @@ void loop() {
   Using a rising edge trigger prevents false starts or erratic displays. 
   It also centers the waveform, the waveform starts at a consistent point each cycle.
   */
-  int prevValue = analogRead(ANALOG_INPUT);
+int prevValue = getAnalog(A0);  // get Analog value
+  // Serial.println(currentValue);
   int currentValue;
   // Wait for a rising edge crossing TRIGGER_LEVEL
   while (true) {
@@ -57,13 +91,16 @@ void loop() {
   int graphHeight = SCREEN_HEIGHT - 10;
   int lastX = 0;
   int lastY = map(currentValue, 0, 1023, graphHeight, 0);
+  int value;
   for (int x = 1; x < SCREEN_WIDTH; x++) {
-    int value = analogRead(ANALOG_INPUT);
+    value = analogRead(ANALOG_INPUT);
     int y = map(value, 0, 1023, graphHeight, 0);          // maps 'value' from [0, 1023] to [54, 0]
     display.drawLine(lastX, lastY, x, y, SSD1306_WHITE);  // Connect points smoothly
     lastX = x;
     lastY = y;
   }
+  Serial.println(value);
 
   display.display();
+  delay(100);
 }
